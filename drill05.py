8e6c6d17d6c145a50e4ea154aa00cc58c63a6970
qkd0863 <qkd7183@naver.com>
2023-10-02 18:01:28 +0900

    hand_arrow 랜덤 위치로 띄우도록 변경

--- drill05.py
+++ drill05.py
@@ -31,7 +31,8 @@ def reset_world():
     x, y = TUK_WIDTH // 2, TUK_HEIGHT // 2
     frame = 0
 
-    hx, hy = TUK_WIDTH - 50, TUK_HEIGHT - 50
+    # hx, hy = TUK_WIDTH - 50, TUK_HEIGHT - 50
+    hx, hy = random.randint(0, TUK_WIDTH), random.randint(0, TUK_HEIGHT)
 
 
 def render_world():
