489090e7a55f398a83b8666f2e1fc803c14cc9b4
qkd0863 <qkd7183@naver.com>
2023-10-02 18:35:01 +0900

    첫번째 손으로 이동하도록 변경

--- drill06.py
+++ drill06.py
@@ -41,7 +41,7 @@ def reset_world():
     action = 3
 
     points = [(100,900),(1200,800),(500,100)]
-    # set_new_target_arrow()
+    set_new_target_arrow()
 
 
 def set_new_target_arrow():
@@ -50,7 +50,7 @@ def set_new_target_arrow():
     global frame
     sx, sy = cx, cy
     # hx, hy = TUK_WIDTH - 50, TUK_HEIGHT - 50
-    hx, hy = random.randint(0, TUK_WIDTH), random.randint(0, TUK_HEIGHT)
+    hx, hy = points[0]
     t = 0.0
     action = 1 if sx < hx else 0
     frame = 0
@@ -73,13 +73,13 @@ def update_world():
 
     frame = (frame + 1) % 8
 
-    # if t <= 1.0:
-    #     cx = (1 - t) * sx + t * hx
-    #     cy = (1 - t) * sy + t * hy
-    #     t += 0.001
-    # else:
-    #     cx, cy = hx, hy
-    #     set_new_target_arrow()
+    if t <= 1.0:
+        cx = (1 - t) * sx + t * hx
+        cy = (1 - t) * sy + t * hy
+        t += 0.001
+    else:
+        cx, cy = hx, hy
+        set_new_target_arrow()
 
 
 open_canvas(TUK_WIDTH, TUK_HEIGHT)
